588fc97e259b0ce2c7e12abce582af7bec4bf15a
vic-3PO <victma2001@gmail.com>
2025-06-09 06:33:17 -0300

    Atualização scrol

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      scrollBehavior: ['responsive'],
-    },
-    plugins: [],
-  };
+    extend: { scrollBehavior: ['responsive'] },
+  },
+  plugins: [],
+};
