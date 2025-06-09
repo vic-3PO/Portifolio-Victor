1a877d02228e88127dc0460376caa4745e536eb2
vic-3PO <victma2001@gmail.com>
2025-06-09 06:36:22 -0300

    Atualização navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            Ali
+            VLM
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
