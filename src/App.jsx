66fcad7e2ac27b7d48f0615076a84cb601e479ad
vic-3PO <victma2001@gmail.com>
2025-06-08 22:25:15 -0300

    Atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "./sections/Hero";
-import About from "./sections/about";
+import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Footer from "./sections/Footer";
